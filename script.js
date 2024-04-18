5170c804a38742fe9bd7aa32bda2cc5e4a68f243
CarlosGherbaz <carlosgherbaz@gmail.com>
2024-04-17 20:19:19 -0400

    fixed bugs and added more functionality to the js validations

--- script.js
+++ script.js
@@ -27,9 +27,78 @@ function checkInputs() {
       }
     });
   }
+
+  if (items[2].value != "") {
+    checkEmailInput();
+  }
+
+  items[2].addEventListener("keyup", () => {
+    checkEmailInput();
+  });
+
+  if (items[4].value != "") {
+    checkPasswordMatch();
+  }
+
+  items[4].addEventListener("keyup", () => {
+    checkPasswordMatch();
+  });
+}
+
+function checkEmailInput() {
+  const emailRegex = /^([a-z\d\.-]+)@([a-z\d-]+)\.([a-z]{2,3})(\.[a-z]{2,3})?$/;
+  const emailErrorText = document.querySelector(".email-error");
+
+  if (!emailInput.value.match(emailRegex)) {
+    emailInput.classList.add("error");
+    emailInput.parentElement.classList.add("error");
+
+    if (emailInput.value != "") {
+      emailErrorText.innerText = "Please enter a valid email";
+    } else {
+      emailErrorText.innerText = "Email can't be empty";
+    }
+  } else {
+    emailInput.classList.remove("error");
+    emailInput.parentElement.classList.remove("error");
+  }
+}
+
+function checkPasswordMatch() {
+  const pwdErrorText = document.querySelector(".confirm-error");
+
+  if (passwordValidation.value !== passwordInput.value) {
+    passwordValidation.classList.add("error");
+    passwordValidation.parentElement.classList.add("error");
+
+    if (passwordValidation.value !== "") {
+      pwdErrorText.innerText = "Passwords don't match";
+    } else {
+      pwdErrorText.innerText = "Confirmation can't be empty";
+    }
+  } else {
+    passwordValidation.classList.remove("error");
+    passwordValidation.parentElement.classList.remove("error");
+  }
 }
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
   checkInputs();
+
+  if (
+    !nameInput.classList.contains("error") &&
+    !emailInput.classList.contains("error") &&
+    !lastNameInput.classList.contains("error") &&
+    !passwordInput.classList.contains("error") &&
+    !passwordValidation.classList.contains("error")
+  ) {
+    Swal.fire({
+      title: "Good job!",
+      text: "Your account is created!",
+      icon: "success",
+    });
+  }
+  form.reset();
+  return false;
 });
